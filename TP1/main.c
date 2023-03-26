807fadb5c54b0fc6e7060e2566ae8746083f2098
Antoine <antoine.montier@sfr.fr>
2023-03-26 15:05:48 +0200

    segfault > solutionner en initialisant la structure de stockage

--- TP1/main.c
+++ TP1/main.c
@@ -19,13 +19,45 @@ typedef struct{
     int nb_action;
 } Stockage;
 
+
+/// @brief ici on verifie que les 3 premiers char, peut etre utile de verifier le mot dans son entierete
+/// @param c 
+/// @return 
+int preconds(char*c){
+    if(c == NULL) return 0;
+    if(c[0] != '\0' && c[0] == 'p' && c[1] != '\0' && c[1] == 'r' && c[2] != '\0' && c[2] == 'e') return 1;
+    return 0;
+}
+
+int add(char*c){
+    if(c == NULL) return 0;
+    if(c[0] != '\0' && c[0] == 'a' && c[1] != '\0' && c[1] == 'd' && c[2] != '\0' && c[2] == 'd') return 1;
+    return 0;
+}
+
+int delete(char*c){
+    if(c == NULL) return 0;
+    if(c[0] != '\0' && c[0] == 'd' && c[1] != '\0' && c[1] == 'e' && c[2] != '\0' && c[2] == 'l') return 1;
+    return 0;
+}
+
+int action(char*c){
+    if(c == NULL) return 0;
+    if(c[0] != '\0' && c[0] == 'a' && c[1] != '\0' && c[1] == 'c' && c[2] != '\0' && c[2] == 't') return 1;
+    return 0;
+}
+
+
+
 int main(){
-    
+
 
     // ============= enregistrer dans un double tableau les lignes du fichier
     char**blocs = lecture("blocs.txt");
 
     Stockage st_blocs[40];
+
+
     int bloc_actuel = 0;
     char tmp;
 
@@ -50,48 +82,53 @@ int main(){
                 tmp = blocs[i][char_actuel_doc];
             }
             bloc_actuel++;
-        }else if(add(blocs[i])){
-
-
-
-
-
-
-
-
-
-
-
-        }else if(action(blocs[i])){
-
-
-
-
-
-
-
-
-
-        }else if(delete(blocs[i])){
-
-
-
-
-
-
-
-
-
-
-        }else{
-            // start, finish, ou ****
         }
 
 
+        else if(add(blocs[i])){
+            printf("la ligne %d commence par add\n", i);
+            int char_actuel_doc = 9;
+            tmp = '\0';
+            while(tmp != '\n'){
+                int char_actuel_add = 0;
+                while((tmp = blocs[i][char_actuel_doc]) != ','){
+                    st_blocs[bloc_actuel].add[  st_blocs[bloc_actuel].nb_add ][char_actuel_add] = tmp;
+                    char_actuel_doc++;
+                    char_actuel_add++;
+                    printf("tmp = %c  ligne de doc = %d  colone du doc = %d  nb_add enregistre = %d\n", tmp, i, char_actuel_doc, st_blocs[bloc_actuel].nb_add);
+                }
+                st_blocs[bloc_actuel].add[  st_blocs[bloc_actuel].nb_add ][char_actuel_add] = '\0';
+                printf("tmp = %c  ligne de doc = %d  colone du doc = %d  nb_add enregistre = %d\n", st_blocs[bloc_actuel].add[  st_blocs[bloc_actuel].nb_add ][char_actuel_add], i, char_actuel_doc, st_blocs[bloc_actuel].nb_add);
+                printf("\n");
+                st_blocs[bloc_actuel].nb_add++;
+                char_actuel_doc++;
+                tmp = blocs[i][char_actuel_doc];
+            }
+            bloc_actuel++;
+        }   
+    
 
-
-
-    }
+    printf("la ligne %d commence par add\n", i);
+            int char_actuel_doc = 9;
+            tmp = '\0';
+            while(tmp != '\n'){
+                int char_actuel_add = 0;
+                while((tmp = blocs[i][char_actuel_doc]) != ','){
+                    st_blocs[bloc_actuel].add[  st_blocs[bloc_actuel].nb_add ][char_actuel_add] = tmp;
+                    char_actuel_doc++;
+                    char_actuel_add++;
+                    printf("tmp = %c  ligne de doc = %d  colone du doc = %d  nb_add enregistre = %d\n", tmp, i, char_actuel_doc, st_blocs[bloc_actuel].nb_add);
+                }
+                st_blocs[bloc_actuel].add[  st_blocs[bloc_actuel].nb_add ][char_actuel_add] = '\0';
+                printf("tmp = %c  ligne de doc = %d  colone du doc = %d  nb_add enregistre = %d\n", st_blocs[bloc_actuel].add[  st_blocs[bloc_actuel].nb_add ][char_actuel_add], i, char_actuel_doc, st_blocs[bloc_actuel].nb_add);
+                printf("\n");
+                st_blocs[bloc_actuel].nb_add++;
+                char_actuel_doc++;
+                tmp = blocs[i][char_actuel_doc];
+            }
+            bloc_actuel++;
+        }   
+        
 
 
 
@@ -118,7 +155,7 @@ int main(){
 
     // for(int i = 0 ; i < BLOCS_LENGHT ; ++i)
     //     printf("%s", blocs[i]);
-    
+
     //============= resoudre les problemes de blocs
 
 
@@ -130,14 +167,6 @@ int main(){
     return 0;
 }
 
-/// @brief ici on verifie que les 3 premiers char, peut etre utile de verifier le mot dans son entierete
-/// @param c 
-/// @return 
-int preconds(char*c){
-    if(c == NULL) return 0;
-    if(c[0] != '\0' && c[0] == 'p' && c[1] != '\0' && c[1] == 'r' && c[2] != '\0' && c[2] == 'e') return 1;
-    return 0;
-}
 
 
 char** lecture(const char* filename) {
