f7646264c0f5c8d27bb1d950c99e555cc455d441
Antoine <antoine.montier@sfr.fr>
2023-04-20 10:49:36 +0200

    fichier riviere cree + resolution fonctionne

---
+++ TP1bis/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void ecrire_regles(char* filename, int nb_blocs);
+
+
+
+
+
+
+int main(){
+
+
+    return 0;
+}
+
+void ecrire_regles(char* filename, int nb_blocs){
+    if(nb_blocs < 1) return;
+    FILE * f = fopen(filename,"w");
+
+    
+
+
+
+
+
+
+
+
+    fclose(f);
+}
